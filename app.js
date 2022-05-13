3bcc92b1852ce65d460c47a1c8d6c4e2c30c187e
Nick Jonescu <nicholasjonescu@gmail.com>
2022-05-13 06:28:53 -0400

    getting closer, still can't string together operations

--- app.js
+++ app.js
@@ -1,60 +1,116 @@
-const displayText = document.querySelector('.display')
+    // declare variables
+let displayText = document.querySelector('.main-display')
 const numberBtns = Array.from(document.querySelectorAll('.number'))
 const operands = document.querySelectorAll('.operand')
 const clearBtn = document.querySelector('.clear-btn')
 const computeBtn = document.querySelector('.equals-btn')
-let firstOperand = ''
-let secondOperand = ''
+let currentOperator = document.querySelector('.current-operator')
+let firstOperand = document.querySelector('.first-operand')
+let operator = ''
 
+displayText.textContent = '0'
+
+    // clear display
 function clear() {
     displayText.textContent = ''
+    firstOperand.textContent = ''
+    currentOperator.textContent = ''
 }
 
 clearBtn.addEventListener('click', clear);
 
+    // add functionality to buttons
 numberBtns.forEach(button => {
     button.addEventListener('click', function() {
-        appendNumber(button.textContent);
+        if(displayText.textContent == '0') {
+            clear();
+        }
+        append(button.textContent)
     })
 });
 
 operands.forEach(operand => {
     operand.addEventListener('click', function() {
+        currentOperator.textContent = operand.textContent
+        operator = ''
 
+        if(firstOperand.textContent == '') {
+         firstOperand.textContent += displayText.textContent;
+         displayText.textContent = ''
+        }   
+        
+        if(displayText.textContent != '') {
+            operate(currentOperator.textContent, firstOperand.textContent, displayText.textContent)
+            displayText.textContent = operator
+        } 
     })
 });
 
-function appendNumber(number) {
-    displayText.textContent += number
+function check() {
+    operate(currentOperator.textContent, firstOperand.textContent, displayText)
 }
 
+    // make the equals button work
+computeBtn.addEventListener('click', function() {
+    operate(currentOperator.textContent, firstOperand.textContent, displayText.textContent)
+    firstOperand.textContent = ''
+})
+
+    // concatenate strings of numbers on display
+function append(string) {
+    displayText.textContent += string
+}
+
+    // operations
 function add(a,b) {
-    return a + b;
+    result = a + b;
+    displayText.textContent = result;
+    firstOperand.textContent = result
+    currentOperator.textContent = ''
+    return result;
 }
 
 function subtract(a,b) {
-    return a - b;
+    result = a - b;
+    displayText.textContent = result;
+    firstOperand.textContent = result
+    currentOperator.textContent = ''
+    return result;
 }
 
 function multiply(a,b) {
-    return a * b;
+    result = a * b;
+    displayText.textContent = result;
+    firstOperand.textContent = result
+    currentOperator.textContent = ''
+    return result;
 }
 
 function divide(a,b) {
-    return a / b;
+    result = a / b;
+    displayText.textContent = result;
+    firstOperand.textContent = result
+    currentOperator.textContent = ''
+    return result;
 }
 
+    // compute
 function operate(operand, a, b) {
-
+    a = Number(a)
+    b = Number(b)
     switch(operand) {
         case "+": 
-        return add(a, b)
+        console.log(add(a, b))
+        break;
         case "-": 
-        return subtract(a,b)
+        console.log(subtract(a,b))
+        break;
         case "*":
-        return multiply(a,b)
+        console.log(multiply(a,b))
+        break;
         case "/":
-        return divide(a,b)
+        console.log(divide(a,b))
+        break;
         default: 
         return null;
     }
