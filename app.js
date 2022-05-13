1303dc87fc8da129226d5ffe7c935e9a719c56a2
Nick Jonescu <nicholasjonescu@gmail.com>
2022-05-13 08:06:42 -0400

    added backspace and decimal functionality

--- app.js
+++ app.js
@@ -1,97 +1,101 @@
-    // declare variables
-let displayText = document.querySelector('.main-display')
-const numberBtns = Array.from(document.querySelectorAll('.number'))
+const decimalBtn = document.getElementById('decimal')
+const numberBtns = document.querySelectorAll('.number')
 const operands = document.querySelectorAll('.operand')
 const clearBtn = document.querySelector('.clear-btn')
 const computeBtn = document.querySelector('.equals-btn')
+const backspaceBtn = document.querySelector('.back-space')
+let displayText = document.querySelector('.main-display')
 let currentOperator = document.querySelector('.current-operator')
 let firstOperand = document.querySelector('.first-operand')
-let operator = ''
-
 displayText.textContent = '0'
 
-    // clear display
+
+//CLEAR DISPLAY
+clearBtn.addEventListener('click', clear);
+    
 function clear() {
-    displayText.textContent = ''
+    displayText.textContent = '0'
     firstOperand.textContent = ''
     currentOperator.textContent = ''
+    decimalBtn.disabled = false;
 }
 
-clearBtn.addEventListener('click', clear);
 
-    // add functionality to buttons
+// CONCATENATE NUMBERS TO DISPLAY
+function append(string) {
+displayText.textContent += string
+}
+
 numberBtns.forEach(button => {
     button.addEventListener('click', function() {
         if(displayText.textContent == '0') {
-            clear();
+            displayText.textContent = ''
+        } else if(displayText.className.includes('answer-exists')) {
+            displayText.classList.toggle('answer-exists')
+            displayText.textContent = ''
         }
         append(button.textContent)
     })
 });
 
+
 operands.forEach(operand => {
     operand.addEventListener('click', function() {
-        currentOperator.textContent = operand.textContent
-        operator = ''
-
         if(firstOperand.textContent == '') {
-         firstOperand.textContent += displayText.textContent;
-         displayText.textContent = ''
-        }   
-        
-        if(displayText.textContent != '') {
-            operate(currentOperator.textContent, firstOperand.textContent, displayText.textContent)
-            displayText.textContent = operator
-        } 
+            firstOperand.textContent = displayText.textContent
+            displayText.textContent = ''
+            currentOperator.textContent = operand.textContent;
+        } else if(firstOperand.textContent != '' && currentOperator.textContent != '') {
+             operate(currentOperator.textContent, firstOperand.textContent, displayText.textContent)
+             firstOperand.textContent = displayText.textContent
+             currentOperator.textContent = operand.textContent;
+             displayText.textContent = ''
+        }
     })
 });
 
-function check() {
-    operate(currentOperator.textContent, firstOperand.textContent, displayText)
-}
+// CHECK FOR DECIMAL
+decimalBtn.addEventListener('click', function() {
+    decimalBtn.disabled = true;
+})
 
-    // make the equals button work
+// MAKE EQUALS BUTTON COMPUTE
 computeBtn.addEventListener('click', function() {
     operate(currentOperator.textContent, firstOperand.textContent, displayText.textContent)
     firstOperand.textContent = ''
+    currentOperator.textContent = ''
+    displayText.classList.add('answer-exists')
 })
 
-    // concatenate strings of numbers on display
-function append(string) {
-    displayText.textContent += string
+//  BACKSPACE BUTTON
+function test(){
+    displayText.textContent = displayText.textContent.slice(0, -1)
 }
+backspaceBtn.addEventListener('click', test)
 
-    // operations
+// OPERATIONS
 function add(a,b) {
-    result = a + b;
-    displayText.textContent = result;
-    firstOperand.textContent = result
-    currentOperator.textContent = ''
-    return result;
+    let answer = a + b; 
+    displayText.textContent = answer;
+    return answer;
 }
 
 function subtract(a,b) {
-    result = a - b;
-    displayText.textContent = result;
-    firstOperand.textContent = result
-    currentOperator.textContent = ''
-    return result;
+    let answer = a - b;
+    displayText.textContent = answer;
+    return answer;
 }
 
 function multiply(a,b) {
-    result = a * b;
-    displayText.textContent = result;
-    firstOperand.textContent = result
-    currentOperator.textContent = ''
-    return result;
+    let answer = a * b;
+    displayText.textContent = answer;
+    return answer;
 }
 
 function divide(a,b) {
-    result = a / b;
-    displayText.textContent = result;
-    firstOperand.textContent = result
-    currentOperator.textContent = ''
-    return result;
+    let answer = a / b;
+    displayText.textContent = answer;
+    return answer;
 }
 
     // compute
